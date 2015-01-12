736c67dc0becf6d33188a728b713f5d65926d798
berkuhlee <berkuhlee@gmail.com>
2015-01-11 20:32:26 -0800

    subchat

--- basicBot.js
+++ basicBot.js
@@ -1415,7 +1415,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat("https://plugcubed.net/Alpha");
+                        API.sendChat(subchat("https://plugcubed.net/Alpha"));
                     }
                 }
             },
